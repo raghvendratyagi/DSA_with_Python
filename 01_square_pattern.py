389d09c13a5d0be159eba85366956278b6023963
RaghvendraTyagi <raghvendra.tyagi.csel.2020@miet.ac.in>
2023-06-05 00:31:23 +0530

    first commit

---
+++ 01_square_pattern.py
@@ -0,0 +1,68 @@
+'''****
+   ****
+   ****
+   ****'''
+
+n=int(input())
+i=1
+while i<=n:
+  j=1
+  while j<=n:
+    print("*",end=" ")
+    j=j+1
+  print()
+  i=i+1
+
+
+
+
+  '''1111
+     2222
+     3333
+     4444
+  '''
+
+  n=int(input())
+i=1
+while i<=n:
+  j=1
+  while j<=n:
+    print(i,end=" ")
+    j=j+1
+  print()
+  i=i+1
+
+  '''
+  1234
+  1234
+  1234
+  1234
+  '''
+
+n=int(input())
+i=1
+while i<=n:
+  j=1
+  while j<=n:
+    print(j,end=" ")
+    j=j+1
+  print()
+  i=i+1
+
+
+
+  '''
+  4321
+  4321
+  4321
+  4321
+  '''
+n=int(input())
+i=1
+while i<=n:
+  j=1
+  while j<=n:
+    print(n-j+1,end=" ")
+    j=j+1
+  print()
+  i=i+1
